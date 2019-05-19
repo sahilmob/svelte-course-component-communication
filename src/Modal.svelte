321b445fbfbb9ff05c8626b3f7eac2537a4a6a8e
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-19 11:36:19 +0300

    component lifecycle hooks examples

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,9 +1,39 @@
 <script>
-  import { createEventDispatcher } from "svelte";
+  import {
+    createEventDispatcher,
+    onMount,
+    onDestroy,
+    beforeUpdate,
+    afterUpdate
+  } from "svelte";
 
   const dispatch = createEventDispatcher();
 
   let agreed = false;
+  let autoscroll = false;
+
+  onMount(() => {
+    console.log("OnMount");
+  });
+
+  onDestroy(() => {
+    console.log("OnDestroy");
+  });
+
+  beforeUpdate(() => {
+    console.log("beforeUpdate");
+    autoscroll = agreed;
+  });
+
+  afterUpdate(() => {
+    console.log("afterUpdate");
+    if (autoscroll) {
+      const modal = document.querySelector(".modal");
+      modal.scrollTo(0, modal.scrollHeight);
+    }
+  });
+
+  console.log("Script executed");
 </script>
 
 <style>
@@ -23,7 +53,7 @@
     top: 10vh;
     left: 10%;
     width: 80%;
-    max-height: 80vh;
+    max-height: 15vh;
     background: white;
     border-radius: 5px;
     z-index: 100;
